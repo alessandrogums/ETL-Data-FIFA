c56cb61bbddc224d35e152d454b64efcfd8ae051
Alessandro Gums <alessandro.gums@clicksign.com>
2022-09-06 09:14:59 -0300

    mudança na classe load_csvs

--- LoadCsvs.py
+++ LoadCsvs.py
@@ -1,13 +1,19 @@
 import os
 
+#classe responsável por carregar os csvs dos dfs gerados
+
 class LoadCsvs:
-    def __init__(self,dataframe):
+    def __init__(self,dataframe,new_folder,name_dataframe,folder_name):
         self.dataframe=dataframe
-    
-    def load_csv(self,name_dataframe,new_folder):
-        path=f'../ETL-Data-FIFA/{new_folder}'
+        self.new_folder=new_folder
+        self.name_dataframe=name_dataframe
+        self.folder_name=folder_name
+
+    @property
+    def load_csv(self):
+        path=f'../{self.folder_name}/{self.new_folder}'
         if not os.path.exists(path=path):
             os.makedirs(path)
         
-        return self.dataframe.to_csv(f'{new_folder}/{name_dataframe}.csv')
+        return self.dataframe.to_csv(f'{self.new_folder}/{self.name_dataframe}.csv')
             
